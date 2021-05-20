51b9e88f29dc8f1a176b1eaf051df48bf83ffe90
Minwoo Na <natorque5552@naver.com>
2021-05-20 23:52:14 +0900

    21.05.20 #2 앨범, 사진 회원별 CRUD 기능 추가

---
+++ photo/forms.py
@@ -0,0 +1,6 @@
+from django.forms import inlineformset_factory  
+from photo.models import Album, Photo  
+
+
+PhotoInlineFormSet = inlineformset_factory(Album, Photo, fields=['image', 'title', 'description'], extra=2)
+
--- photo/urls.py
+++ photo/urls.py
@@ -18,4 +18,28 @@ urlpatterns = [
     # Example: /photo/photo/99/
     path('photo/<int:pk>/', views.PhotoDV.as_view(), name='photo_detail'),
 
+    # Example: /photo/album/add/
+    path('album/add/', views.AlbumPhotoCV.as_view(), name='album_add'),
+
+    # Example: /photo/album/change/
+    path('album/change/', views.AlbumChangeLV.as_view(), name='album_change'),
+
+    # Example: /photo/album/99/update/
+    path('album/<int:pk>/update/', views.AlbumPhotoUV.as_view(), name='album_update'),
+
+    # Example: /photo/album/99/delete/
+    path('album/<int:pk>/delete/', views.AlbumDelV.as_view(), name='album_delete'),
+
+    # Example: /photo/photo/add/
+    path('photo/add/', views.PhotoCV.as_view(), name='photo_add'),
+
+    # Example: /photo/photo/change/
+    path('photo/change/', views.PhotoChangeLV.as_view(), name='photo_change'),
+
+    # Example: /photo/photo/99/update/
+    path('photo/<int:pk>/update/', views.PhotoUV.as_view(), name='photo_update'),
+
+    # Example: /photo/photo/99/delete/
+    path('photo/<int:pk>/delete/', views.PhotoDelV.as_view(), name='photo_delete'),
+
 ]
